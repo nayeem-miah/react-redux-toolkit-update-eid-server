fe64d811baf9c1cbdd296cb2bddcc13c1efd0561
nayeem-miah <nayeem5113a@gmail.com>
2025-03-10 18:01:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World! from eid-spectal");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
