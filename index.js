cbfa3efa4f7d68492b30ca4e97f01bec91ea5f22
nayeem-miah <nayeem5113a@gmail.com>
2025-03-21 16:23:21 +0600

    deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bcrypt = require("bcryptjs");
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173","https://eid-special-react-update.netlify.app"],
   credentials: true, 
 }));
 app.use(express.json());
@@ -20,8 +20,8 @@ app.use(cookieParser());
 
 
 // MONGODB
-  const uri ='mongodb://localhost:27017'
-  // const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  // const uri ='mongodb://localhost:27017'
+  const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bomlehy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
   const client = new MongoClient(uri, {
